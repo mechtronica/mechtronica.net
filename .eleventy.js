e08eff4e1f681ab53701cbd13827cb0c81cc78c5
Icosahunter <anzelknight@gmail.com>
2023-04-22 22:00:52 -0500

    Add keyword shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -7,16 +7,19 @@ module.exports = function(eleventyConfig) {
       linkify: true
     }
     const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
-    eleventyConfig.addPassthroughCopy("halfmoonui");
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("fonts");
-    eleventyConfig.addPassthroughCopy("character-sheets/*.pdf");
-    eleventyConfig.addPassthroughCopy("mechdb/parts_database.json");
-    eleventyConfig.addPassthroughCopy("jsonpath-0.8.0.js");
-    eleventyConfig.addPassthroughCopy("custom-variables.css");
+    eleventyConfig.addPassthroughCopy('halfmoonui');
+    eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('fonts');
+    eleventyConfig.addPassthroughCopy('character-sheets/*.pdf');
+    eleventyConfig.addPassthroughCopy('mechdb/parts_database.json');
+    eleventyConfig.addPassthroughCopy('jsonpath-0.8.0.js');
+    eleventyConfig.addPassthroughCopy('custom-variables.css');
     eleventyConfig.setNunjucksEnvironmentOptions({
       throwOnUndefined: true,
       autoescape: false, // warning: don’t do this!
     });
     eleventyConfig.setLibrary('md', markdownLib);
+    eleventyConfig.addShortcode('keyword', function(word, glossary) {
+      return '<b class="keyword" data-toggle="tooltip" data-title="' + glossary[word] + '">' + word + '</b>'
+    });
 };
